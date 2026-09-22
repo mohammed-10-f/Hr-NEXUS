c41adf36c54bb143496b5cfde5995edf4320e872
mohammed-10-f <mohammed.f.otb1@gmail.com>
2026-09-22 15:16:05 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: 'dist/client', emptyOutDir: true },
+  build: { outDir: 'dist', emptyOutDir: true },
   server: { port: 5173 }
 });
